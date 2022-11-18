ca2b0d7fec6d24409b11d3d082b53996f73b2e40
manu310397 <manojb912@gmail.com>
2022-11-18 09:07:57 +0530

    Configured Eureka discovery service

---
+++ src/main/java/com/manoj/springms/SpringMsEurekaDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.manoj.springms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringMsEurekaDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsEurekaDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manoj/springms/SpringMsEurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.springms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMsEurekaDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
